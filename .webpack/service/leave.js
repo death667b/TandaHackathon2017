a6e3afe4469298726bf9d8d1b39e7331ae782ea0
death667b <idaniel@spinnys.com>
2017-10-28 16:12:30 +1000

    why

--- .webpack/service/leave.js
+++ .webpack/service/leave.js
@@ -78,26 +78,40 @@ var _stringify = __webpack_require__(1);
 
 var _stringify2 = _interopRequireDefault(_stringify);
 
+var _maxSafeInteger = __webpack_require__(2);
+
+var _maxSafeInteger2 = _interopRequireDefault(_maxSafeInteger);
+
 exports.main = main;
 
-var _uuid = __webpack_require__(2);
+var _uuid = __webpack_require__(3);
 
 var _uuid2 = _interopRequireDefault(_uuid);
 
-var _awsSdk = __webpack_require__(3);
+var _awsSdk = __webpack_require__(4);
 
 var _awsSdk2 = _interopRequireDefault(_awsSdk);
 
-var _requestPromise = __webpack_require__(4);
+var _requestPromise = __webpack_require__(5);
 
 var _requestPromise2 = _interopRequireDefault(_requestPromise);
+
+var _twilio = __webpack_require__(6);
+
+var _twilio2 = _interopRequireDefault(_twilio);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 _awsSdk2.default.config.update({ region: "ap-southeast-2" });
 
+var accountID = 'AC1d5cf343f82ccd0c15e44feb15fa6dcc';
+var accountAuth = 'afa732887930ef637618e340063d2030';
+
+var client = new _twilio2.default(accountID, accountAuth);
+
 function main(event, context, callback) {
-    var eventBody = event.payload.body;
+    //const eventBody = event.payload.body;
+
     var date = new Date();
     var startDate = date.getYear() - 100 + '-' + date.getMonth() + '-' + date.getDate();
     var endDate = date.getYear() - 100 + 1 + '-' + date.getMonth() + '-' + date.getDate();
@@ -115,23 +129,45 @@ function main(event, context, callback) {
     };
 
     (0, _requestPromise2.default)(rpOptions).then(function (requestEvents) {
-        console.log('User has %d repos', requestEvents.length);
+        // console.log('User has %d requests', requestEvents.length);
 
         var pending = 0,
             approved = 0,
-            declined = 0;
+            declined = 0,
+            minOld = _maxSafeInteger2.default;
+        //let sendNewSMS = false;
 
         requestEvents.forEach(function (reqEvent) {
-            if (reqEvent.status === 'rejected') declined++;
-            if (reqEvent.status === 'approved') approved++;
-            if (reqEvent.status === 'pending') pending++;
+            //if (reqEvent.status === 'rejected') declined++;
+            //if (reqEvent.status === 'approved') approved++;
+            if (reqEvent.status === 'pending') {
+                pending++;
+
+                var editedDate = parseInt(reqEvent.updated_at) * 1000;
+                var thisTime = (date.getTime() - editedDate) / 1000 / 60;
+                if (minOld > thisTime) minOld = thisTime;
+            }
         });
 
-        var returnBody = "There is " + pending + " pending, " + approved + " approved and " + declined + " declined requests for leave";
+        if (pending === 1 || minOld >= 10) {
+            client.messages.create({
+                body: 'There is at least 1 leave request to review',
+                to: '+61423022733',
+                from: '+61447082035'
+            }).then(function (msg) {
+                var response = {
+                    statusCode: 200,
+                    body: 'new sms sent'
+                };
+                callback(null, response);
+            });
+        }
+        //const returnBody = `${minOld} minutes old, There is ${pending} pending, ${approved} approved and ${declined} declined requests for leave`;
+
 
         var response = {
             statusCode: 200,
-            body: returnBody
+            body: '200 but nothing sent'
         };
         callback(null, response);
     }).catch(function (err) {
@@ -153,19 +189,31 @@ module.exports = require("babel-runtime/core-js/json/stringify");
 /* 2 */
 /***/ (function(module, exports) {
 
-module.exports = require("uuid");
+module.exports = require("babel-runtime/core-js/number/max-safe-integer");
 
 /***/ }),
 /* 3 */
 /***/ (function(module, exports) {
 
-module.exports = require("aws-sdk");
+module.exports = require("uuid");
 
 /***/ }),
 /* 4 */
 /***/ (function(module, exports) {
 
+module.exports = require("aws-sdk");
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports) {
+
 module.exports = require("request-promise");
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports) {
+
+module.exports = require("twilio");
 
 /***/ })
 /******/ ])));
--- leave.js
+++ leave.js
@@ -1,12 +1,17 @@
 import uuid from "uuid";
 import AWS from "aws-sdk";
 import rp from "request-promise";
+import twilio from 'twilio';
 
 AWS.config.update({ region: "ap-southeast-2" });
 
+var accountID = 'AC1d5cf343f82ccd0c15e44feb15fa6dcc';
+var accountAuth = 'afa732887930ef637618e340063d2030';
+
+var client = new twilio(accountID, accountAuth);
 
 export function main(event, context, callback) {
-    const eventBody = event.payload.body;
+    //const eventBody = event.payload.body;
     
     const date = new Date();
     const startDate = (date.getYear() - 100 ) + '-' + date.getMonth() + '-' + date.getDate();
@@ -26,21 +31,43 @@ export function main(event, context, callback) {
 
     rp(rpOptions)
         .then(function (requestEvents) {
-            console.log('User has %d repos', requestEvents.length);
+            // console.log('User has %d requests', requestEvents.length);
 
-            let pending = 0, approved = 0, declined = 0;
+            let pending = 0, approved = 0, declined = 0, minOld = Number.MAX_SAFE_INTEGER;
+            //let sendNewSMS = false;
 
             requestEvents.forEach(reqEvent => {
-                if (reqEvent.status === 'rejected') declined++;
-                if (reqEvent.status === 'approved') approved++;
-                if (reqEvent.status === 'pending') pending++;
+                //if (reqEvent.status === 'rejected') declined++;
+                //if (reqEvent.status === 'approved') approved++;
+                if (reqEvent.status === 'pending') {
+                    pending++;
+
+                    const editedDate = parseInt(reqEvent.updated_at)*1000;
+                    let thisTime = (date.getTime() - editedDate) / 1000 / 60;
+                    if (minOld > thisTime) minOld = thisTime;
+                }
             })
 
-            const returnBody = `There is ${pending} pending, ${approved} approved and ${declined} declined requests for leave`;
+            if (pending === 1 || minOld>=10) {
+                client.messages.create({
+                    body: 'There is at least 1 leave request to review',
+                    to: '+61423022733',
+                    from: '+61447082035'
+                  }).then( msg => {
+                      const response = {
+                          statusCode: 200,
+                          body: 'new sms sent'
+                      };
+                      callback(null, response);
+                  });
+            }
+            //const returnBody = `${minOld} minutes old, There is ${pending} pending, ${approved} approved and ${declined} declined requests for leave`;
+            
+
 
             const response = {
                 statusCode: 200,
-                body: returnBody
+                body: '200 but nothing sent'
             };
             callback(null, response);
         })
